36e363b2c62c2d1e600852442e19a8f16596e87f
friday <friday0701@mail.ru>
2023-02-07 12:47:39 +0400

    resultmodel edit

--- Model/ResultModel.java
+++ Model/ResultModel.java
@@ -29,10 +29,6 @@ public class ResultModel extends CalcModel {
         return result;
     }
 
-//    @Override
-//    public int operation() {
-//        return 0;
-//    }
 
     @Override
     public void setX(int value) {
